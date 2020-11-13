6884b65f935f2bd0d264104bd73593238c141c94
soumya240806 <72087897+soumya240806@users.noreply.github.com>
2020-11-13 21:34:28 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,26 @@
+class Ball{
+
+    constructor(x, y, radius) {
+        var options = {
+
+            restitution: 0.8,
+            friction: 1.0,
+            density:1.0,
+
+        }
+
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+        World.add(world, this.body);
+    }
+
+    display() {
+
+        var pos = this.body.position;
+        stroke(176,219,83);
+        strokeWeight(2);
+        ellipseMode(CENTER);
+        ellipse(pos.x,pos.y,this.radius);
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# soumya240806-towersiege.github.io
+# p5.play-boilerplate
+Boiler plate for p5.play
